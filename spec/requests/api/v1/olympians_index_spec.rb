0179745952cc5b6b97499c83fe0043c2389871da
Patrick Duvall <duvallp208@gmail.com>
2019-09-16 15:38:32 -0600

    Get info throgh pipe, need refactor to serializer

--- spec/requests/api/v1/olympians_index_spec.rb
+++ spec/requests/api/v1/olympians_index_spec.rb
@@ -3,11 +3,11 @@ require "rails_helper"
 describe "Olympians Index do" do
 
   before :each do
-    o1, o2, o3 = create_list(:olympian, 3)
-    s1, s2, s3 = create_list(:sport, 3)
-    create(:olympian_sport, olympian_id: o1.id, sport_id: s1.id)
-    create(:olympian_sport, olympian_id: o2.id, sport_id: s2.id)
-    create(:olympian_sport, olympian_id: o3.id, sport_id: s3.id)
+    @o1, @o2, @o3 = create_list(:olympian, 3)
+    @s1, @s2, @s3 = create_list(:sport, 3)
+    create(:olympian_sport, olympian_id: @o1.id, sport_id: @s1.id)
+    create(:olympian_sport, olympian_id: @o2.id, sport_id: @s2.id)
+    create(:olympian_sport, olympian_id: @o3.id, sport_id: @s3.id)
   end
 
   it "gets all olympians" do
@@ -16,10 +16,10 @@ describe "Olympians Index do" do
     expect(response.status).to eq(200)
     data = JSON.parse(response.body)
     expect(data["olympians"].count).to eq(3)
-    expect(data["olympians"][0]['name']).to eq(o1.name)
-    expect(data["olympians"][0]['team']).to eq(o1.team)
-    expect(data["olympians"][0]['age']).to eq(o1.age)
-    expect(data["olympians"][0]['sport']).to eq(s1.name)
+    expect(data["olympians"][0]['name']).to eq(@o1.name)
+    expect(data["olympians"][0]['team']).to eq(@o1.team)
+    expect(data["olympians"][0]['age']).to eq(@o1.age)
+    expect(data["olympians"][0]['sport']).to eq(@s1.name)
     expect(data["olympians"][0]['total_medals_won']).to eq(0)
   end
 end
