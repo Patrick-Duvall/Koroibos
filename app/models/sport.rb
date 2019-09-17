cc845f41d96c470dfb63ebe6199944da6a11c85d
Patrick Duvall <duvallp208@gmail.com>
2019-09-17 09:02:13 -0600

    Add sport.event_names

--- app/models/sport.rb
+++ app/models/sport.rb
@@ -2,4 +2,8 @@ class Sport < ApplicationRecord
   has_many :olympian_sports
   has_many :olympians, through: :olympian_sports
   has_many :events
+
+  def event_names
+    events.pluck(:name)
+  end
 end
