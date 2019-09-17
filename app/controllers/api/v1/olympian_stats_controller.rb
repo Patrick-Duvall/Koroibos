098b6153d3cf4a218c92dd3d6a9ec0778c83dcf2
Patrick Duvall <duvallp208@gmail.com>
2019-09-16 18:51:41 -0600

    Refactor olympian stats into serializer

--- app/controllers/api/v1/olympian_stats_controller.rb
+++ app/controllers/api/v1/olympian_stats_controller.rb
@@ -1,15 +1,5 @@
 class Api::V1::OlympianStatsController < ApplicationController
   def index
-    retval = {olympian_stats: {
-      total_competing_olympians: Olympian.count,
-      average_age: Olympian.average_age,
-      average_weight: {
-        unit: "kg",
-        male_olympians: Olympian.male_average_weight,
-        female_olympians: Olympian.female_average_weight,
-      }
-      }
-    }
-    render json: retval
+    render json: OlympianStatsSerializer.present_stats
   end
 end
--- spec/requests/api/v1/olympian_stats_spec.rb
+++ spec/requests/api/v1/olympian_stats_spec.rb
@@ -18,14 +18,3 @@ describe "Olympians Index do" do
     expect(data['olympian_stats']["average_weight"]).to include("female_olympians")
   end
 end
-# {
-#     "olympian_stats": {
-#       "total_competing_olympians": 3120
-#       "average_weight:" {
-#         "unit": "kg",
-#         "male_olympians": 75.4,
-#         "female_olympians": 70.2
-#       }
-#       "average_age:" 26.2
-#     }
-#   }
