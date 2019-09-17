15afc32b62acae3eac3707d3747642495f5874ab
Patrick Duvall <duvallp208@gmail.com>
2019-09-17 14:48:17 -0600

    Add case for Cote Divoire and fix olympian model spec

--- app/models/olympian_event.rb
+++ app/models/olympian_event.rb
@@ -3,18 +3,22 @@ class OlympianEvent < ApplicationRecord
   belongs_to :event
 
   def self.gold_count(team)
+    # To avoid sql error on Cote d'Ivoire
+    team = team.gsub("\'", "\'\'")
     joins(:olympian)
     .where("olympians.team like '#{team}'")
     .where("olympian_events.medal like 'Gold'")
     .count
   end
   def self.silver_count(team)
+    team = team.gsub("\'", "\'\'")
     joins(:olympian)
     .where("olympians.team like '#{team}'")
     .where("olympian_events.medal like 'Silver'")
     .count
   end
   def self.bronze_count(team)
+    team = team.gsub("\'", "\'\'")
     joins(:olympian)
     .where("olympians.team like '#{team}'")
     .where("olympian_events.medal like 'Bronze'")
