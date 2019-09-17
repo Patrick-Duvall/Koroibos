ce9a987ffc11f2d8b0b1273db8d096fe698f6d6c
Patrick Duvall <duvallp208@gmail.com>
2019-09-17 17:11:21 -0600

    Add other route that goes to welcome

--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -6,5 +6,9 @@ describe "welcome page do" do
     visit '/'
     expect(page).to have_link("Click Here")
   end
+  it "loads by visiting bad path" do
+    visit '/canyon/fort'
+    expect(page).to have_link("Click Here")
+  end
 
 end
