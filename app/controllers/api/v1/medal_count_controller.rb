2557271f1b8848bc2336130386603f41637823da
Patrick Duvall <duvallp208@gmail.com>
2019-09-17 14:57:30 -0600

    Refactor to serializer

--- app/controllers/api/v1/medal_count_controller.rb
+++ app/controllers/api/v1/medal_count_controller.rb
@@ -1,17 +1,5 @@
 class Api::V1::MedalCountController < ApplicationController
   def index
-    names = Olympian.select(:team).distinct.pluck(:team)
-    response = names.map do |team_name|
-      {
-        name: team_name,
-        medals: {
-          gold: OlympianEvent.gold_count(team_name),
-          silver: OlympianEvent.silver_count(team_name),
-          bronze: OlympianEvent.bronze_count(team_name)
-        }
-      }
-    end
-    # MedalCountSerializer.present_medals
-    render json: {countries: response}
+    render json: MedalCountSerializer.present_medals
   end
 end
